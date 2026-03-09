40b191aa7937a53d3cc502d161b5be407f970a5f
kunal krishnan <61145182+kunalkrishnan@users.noreply.github.com>
2026-03-09 15:33:00 +0530

    Update pyspark_transform.py
    added pyspark_transform logic

--- databricks/pyspark_transform.py
+++ databricks/pyspark_transform.py
@@ -1,1 +1,17 @@
 print("Sample Databricks ETL script")
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName("AzureDataEngineeringPipeline").getOrCreate()
+
+# Read raw data
+df = spark.read.csv("data/sample_data.csv", header=True, inferSchema=True)
+
+# Basic transformation
+clean_df = df.filter(col("amount") > 0)
+
+# Show result
+clean_df.show()
+
+# Write processed data
+clean_df.write.mode("overwrite").parquet("data/processed_sales")
