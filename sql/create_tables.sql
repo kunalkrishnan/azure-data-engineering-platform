0cbb750dd64500b296a3d72cc90224c72e256e01
kunal krishnan <61145182+kunalkrishnan@users.noreply.github.com>
2026-03-09 15:34:40 +0530

    Update create_tables.sql
    added sql query for fact and dimension table.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,13 @@
-CREATE TABLE sales(
-id INT,
-amount FLOAT
+CREATE TABLE fact_sales (
+    order_id INT,
+    customer_id INT,
+    product_id INT,
+    sales_amount FLOAT,
+    order_date DATE
+);
+
+CREATE TABLE dim_customer (
+    customer_id INT,
+    customer_name VARCHAR(100),
+    city VARCHAR(50)
 );
